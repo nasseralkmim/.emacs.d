b7cd95cf6a783d418bb5f8676ca2a9b0d7dd9886
nasseralkmim <nasser.alkmim@gmail.com>
2015-09-25 15:34:30 -0300

    asd

---
+++ elpa/auto-complete-auctex-20140223.958/auto-complete-auctex-autoloads.el
@@ -0,0 +1,16 @@
+;;; auto-complete-auctex-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("auto-complete-auctex.el") (22001 58803
+;;;;;;  616000 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; auto-complete-auctex-autoloads.el ends here
---
+++ elpa/auto-complete-auctex-20140223.958/auto-complete-auctex-pkg.el
@@ -0,0 +1,1 @@
+(define-package "auto-complete-auctex" "20140223.958" "auto-completion for auctex" '((yasnippet "0.6.1") (auto-complete "1.4")))
