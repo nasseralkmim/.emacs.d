371d94d2eb4604cd62ed7b8589420d18c4752439
Nasser Alkmim <nasser.alkmim@gmail.com>
2021-08-19 10:19:33 +0200

    Avoid completion pop up when creating a new line in org-src.

--- init.el
+++ init.el
@@ -150,12 +150,8 @@ frame if FRAME is nil, and to 1 if AMT is nil."
 	'(read-only t cursor-intangible t face minibuffer-prompt))
   (add-hook 'minibuffer-setup-hook #'cursor-intangible-mode)
 
-  ;; 
-  (setq indent-tabs-mode nil)
-
-  ;; Enable indentation+completion using the TAB key.
-  ;; `completion-at-point' is often bound to M-TAB.
-  (setq tab-always-indent 'complete))
+  ;; don't insert tab when indent
+  (setq indent-tabs-mode nil))
 
 (use-package abbrev
   :straight nil
