779945050577918564d054e6f56ef52a5f6f2115
Nasser Alkmim <nasser.alkmim@gmail.com>
2022-10-06 14:24:56 +0200

    add org function to show warnings when executing shell blocks.

--- init.el
+++ init.el
@@ -2704,4 +2704,42 @@ Only if there is more than one window opened."
 (use-package diredfl
   :hook (dired-mode . diredfl-mode))
 
+;; show org-babel error or warning when execute block
+(use-package org-babel-eval-verbose
+  :straight nil
+  :after org
+  :init
+  (defvar org-babel-eval-verbose t
+    "A non-nil value makes `org-babel-eval' display")
+
+  (defun org-babel-eval (command query)
+    "Run COMMAND on QUERY.
+Writes QUERY into a temp-buffer that is processed with
+`org-babel--shell-command-on-region'.  If COMMAND succeeds then return
+its results, otherwise display STDERR with
+`org-babel-eval-error-notify'."
+    (let ((error-buffer (get-buffer-create " *Org-Babel Error*")) exit-code)
+      (with-current-buffer error-buffer (erase-buffer))
+      (with-temp-buffer
+        (insert query)
+        (setq exit-code
+              (org-babel--shell-command-on-region
+               command error-buffer))
+
+        (if (or (not (numberp exit-code)) (> exit-code 0)
+                (and org-babel-eval-verbose (> (buffer-size error-buffer) 0)))
+            (progn
+              (with-current-buffer error-buffer
+                (org-babel-eval-error-notify exit-code (buffer-string)))
+              (save-excursion
+                (when (get-buffer org-babel-error-buffer-name)
+                  (with-current-buffer org-babel-error-buffer-name
+                    (unless (derived-mode-p 'compilation-mode)
+                      (compilation-mode))
+                    ;; Compilation-mode enforces read-only, but Babel expects the buffer modifiable.
+                    (setq buffer-read-only nil))))
+              nil)
+          (buffer-string))))))
+
+
 (message "Start up time %.2fs" (float-time (time-subtract (current-time) my-start-time)))
