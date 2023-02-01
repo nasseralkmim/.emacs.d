f6377f104ae5a75ec36c9b6fad88c98156a5597f
Nasser Alkmim <nasser.alkmim@gmail.com>
2023-02-01 13:26:13 +0100

    Change the eww syntax highlight package

--- init.el
+++ init.el
@@ -447,8 +447,8 @@ frame if FRAME is nil, and to 1 if AMT is nil."
                      :preview-key '(:debounce 5 any))
   ;; use a key for preview from ripgrep and find
   (consult-customize consult-ripgrep consult-find-fd
-                     :preview-key (kbd "M-."))
-  (consult-customize consult-line :preview-key '(:debounce 0 any))
+                     :preview-key '("M-."))
+  (consult-customize consult-line :preview-key 'any)
 
   ;; use 'fd' instead of 'find'
   (defun consult-find-fd (&optional dir initial)
@@ -3201,91 +3201,12 @@ its results, otherwise display STDERR with
   (isearch-mode-map "C-n" 'isearch-repeat-forward)
   (isearch-mode-map "C-p" 'isearch-repeat-backward))
 
-;; used for syntax highlight in eww
-(use-package language-detection
-  :defer t)
-
 ;; highlight code in eww
-(use-package eww-syntax-highlight
-  :straight nil
-  :after eww
+(use-package shr-tag-pre-highlight
+  :after shr
   :init
-  (require 'cl-lib)
-
-  (defun eww-tag-pre (dom)
-    (let ((shr-folding-mode 'none)
-          (shr-current-font 'default))
-      (shr-ensure-newline)
-      (insert (eww-fontify-pre dom))
-      (shr-ensure-newline)))
-
-  (defun eww-fontify-pre (dom)
-    (with-temp-buffer
-      (shr-generic dom)
-      (let ((mode (eww-buffer-auto-detect-mode)))
-        (when mode
-          (eww-fontify-buffer mode)))
-      (buffer-string)))
-
-  (defun eww-fontify-buffer (mode)
-    (delay-mode-hooks (funcall mode))
-    (font-lock-default-function mode)
-    (font-lock-default-fontify-region (point-min)
-                                      (point-max)
-                                      nil))
-
-  (defun eww-buffer-auto-detect-mode ()
-    (let* ((map '((ada ada-mode)
-                  (awk awk-mode)
-                  (c c-mode)
-                  (cpp c++-mode)
-                  (clojure clojure-mode lisp-mode)
-                  (csharp csharp-mode java-mode)
-                  (css css-mode)
-                  (dart dart-mode)
-                  (delphi delphi-mode)
-                  (emacslisp emacs-lisp-mode)
-                  (erlang erlang-mode)
-                  (fortran fortran-mode)
-                  (fsharp fsharp-mode)
-                  (go go-mode)
-                  (groovy groovy-mode)
-                  (haskell haskell-mode)
-                  (html html-mode)
-                  (java java-mode)
-                  (javascript javascript-mode)
-                  (json json-mode javascript-mode)
-                  (latex latex-mode)
-                  (lisp lisp-mode)
-                  (lua lua-mode)
-                  (matlab matlab-mode octave-mode)
-                  (objc objc-mode c-mode)
-                  (perl perl-mode)
-                  (php php-mode)
-                  (prolog prolog-mode)
-                  (python python-mode)
-                  (r r-mode)
-                  (ruby ruby-mode)
-                  (rust rust-mode)
-                  (scala scala-mode)
-                  (shell shell-script-mode)
-                  (smalltalk smalltalk-mode)
-                  (sql sql-mode)
-                  (swift swift-mode)
-                  (visualbasic visual-basic-mode)
-                  (xml sgml-mode)))
-           (language (language-detection-string
-                      (buffer-substring-no-properties (point-min) (point-max))))
-           (modes (cdr (assoc language map)))
-           (mode (cl-loop for mode in modes
-                          when (fboundp mode)
-                          return mode)))
-      (message (format "%s" language))
-      (when (fboundp mode)
-        mode)))
-
-  (setq shr-external-rendering-functions
-        '((pre . eww-tag-pre))))
+  (add-to-list 'shr-external-rendering-functions
+               '(pre . shr-tag-pre-highlight)))
 
 (use-package speedbar 
   :straight (:type built-in))
