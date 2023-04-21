a3a7c8263249539546ae8f804c43ff30b8963eba
Nasser Alkmim <nasser.alkmim@gmail.com>
2023-04-21 08:16:22 +0200

    Try nerd icon with all the icons

--- init.el
+++ init.el
@@ -177,7 +177,7 @@
     (invert-face 'default)))
 
 ;; typeface
-(use-package custom-typefaces
+(use-package custom-typefaces :disabled
   :elpaca nil
   :custom-face 
   ;; victor mono sometimes is nice for comments
@@ -186,7 +186,7 @@
   (font-lock-constant-face ((t (:height 1.05 :family "Iosevka SS12"))))
   ;; outline 4 inherits from comment face... make it oblique instead of italic
   (outline-4 ((t (:inherit font-lock-doc-face))))
-  (default  ((t (:family "Iosevka SS12"))))
+  (default  ((t (:family "Iosevka Nerd Font"))))
   (variable-pitch ((t (:family "Iosevka etoile"))))
   ;; when using variable pitch in org mode, use monospace for code blocks
   (org-block ((t (:family "Iosevka SS12"))))
@@ -526,7 +526,57 @@ frame if FRAME is nil, and to 1 if AMT is nil."
 ;; utility for using icons fonts
 (use-package all-the-icons
   :custom
-  (all-the-icons-scale-factor 0.8))
+  (all-the-icons-scale-factor 1))
+
+(use-package nerd-fonts
+  :elpaca (nerd-fonts :host github :repo "twlz0ne/nerd-fonts.el")
+  :config
+  (setq kind-icon-use-icons nil)
+  (setq kind-icon-mapping
+        `(
+          (array ,(nerd-icons-codicon "nf-cod-symbol_array") :face font-lock-type-face)
+          (boolean ,(nerd-icons-codicon "nf-cod-symbol_boolean") :face font-lock-builtin-face)
+          (class ,(nerd-icons-codicon "nf-cod-symbol_class") :face font-lock-type-face)
+          (color ,(nerd-icons-codicon "nf-cod-symbol_color") :face success)
+          (command ,(nerd-icons-codicon "nf-cod-terminal") :face default)
+          (constant ,(nerd-icons-codicon "nf-cod-symbol_constant") :face font-lock-constant-face)
+          (constructor ,(nerd-icons-codicon "nf-cod-triangle_right") :face font-lock-function-name-face)
+          (enummember ,(nerd-icons-codicon "nf-cod-symbol_enum_member") :face font-lock-builtin-face)
+          (enum-member ,(nerd-icons-codicon "nf-cod-symbol_enum_member") :face font-lock-builtin-face)
+          (enum ,(nerd-icons-codicon "nf-cod-symbol_enum") :face font-lock-builtin-face)
+          (event ,(nerd-icons-codicon "nf-cod-symbol_event") :face font-lock-warning-face)
+          (field ,(nerd-icons-codicon "nf-cod-symbol_field") :face font-lock-variable-name-face)
+          (file ,(nerd-icons-codicon "nf-cod-symbol_file") :face font-lock-string-face)
+          (folder ,(nerd-icons-codicon "nf-cod-folder") :face font-lock-doc-face)
+          (interface ,(nerd-icons-codicon "nf-cod-symbol_interface") :face font-lock-type-face)
+          (keyword ,(nerd-icons-codicon "nf-cod-symbol_keyword") :face font-lock-keyword-face)
+          (macro ,(nerd-icons-codicon "nf-cod-symbol_misc") :face font-lock-keyword-face)
+          (magic ,(nerd-icons-codicon "nf-cod-wand") :face font-lock-builtin-face)
+          (method ,(nerd-icons-codicon "nf-cod-symbol_method") :face font-lock-function-name-face)
+          (function ,(nerd-icons-codicon "nf-cod-symbol_method") :face font-lock-function-name-face)
+          (module ,(nerd-icons-codicon "nf-cod-file_submodule") :face font-lock-preprocessor-face)
+          (numeric ,(nerd-icons-codicon "nf-cod-symbol_numeric") :face font-lock-builtin-face)
+          (operator ,(nerd-icons-codicon "nf-cod-symbol_operator") :face font-lock-comment-delimiter-face)
+          (param ,(nerd-icons-codicon "nf-cod-symbol_parameter") :face default)
+          (property ,(nerd-icons-codicon "nf-cod-symbol_property") :face font-lock-variable-name-face)
+          (reference ,(nerd-icons-codicon "nf-cod-references") :face font-lock-variable-name-face)
+          (snippet ,(nerd-icons-codicon "nf-cod-symbol_snippet") :face font-lock-string-face)
+          (string ,(nerd-icons-codicon "nf-cod-symbol_string") :face font-lock-string-face)
+          (struct ,(nerd-icons-codicon "nf-cod-symbol_structure") :face font-lock-variable-name-face)
+          (text ,(nerd-icons-codicon "nf-cod-text_size") :face font-lock-doc-face)
+          (typeparameter ,(nerd-icons-codicon "nf-cod-list_unordered") :face font-lock-type-face)
+          (type-parameter ,(nerd-icons-codicon "nf-cod-list_unordered") :face font-lock-type-face)
+          (unit ,(nerd-icons-codicon "nf-cod-symbol_ruler") :face font-lock-constant-face)
+          (value ,(nerd-icons-codicon "nf-cod-symbol_field") :face font-lock-builtin-face)
+          (variable ,(nerd-icons-codicon "nf-cod-symbol_variable") :face font-lock-variable-name-face)
+          (t ,(nerd-icons-codicon "nf-cod-code") :face font-lock-warning-face))))
+
+(use-package all-the-icons-nerd-fonts
+  :elpaca (all-the-icons-nerd-fonts :host github :repo "mohkale/all-the-icons-nerd-fonts")
+  :after all-the-icons
+  :demand t
+  :config
+  (all-the-icons-nerd-fonts-prefer))
 
 ;; automatic insert matching pairs and navigation
 ;; highlight matching parens
@@ -2560,7 +2610,7 @@ opening a file from dired. Otherwise just regular dired."
 
 ;; icons for dired
 (use-package all-the-icons-dired
-  :when (display-graphic-p)
+  ;; :when (display-graphic-p)
   :hook
   (dired-mode . (lambda ()
                   ;; don't enable in dired-sidebar
