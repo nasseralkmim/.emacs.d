c4ff2beaa6699d137fdbfbc3617703da2faaa89d
Nasser Alkmim <nasser.alkmim@gmail.com>
2023-05-30 16:36:29 +0200

    Update face multiedit to show visual as well

--- init.el
+++ init.el
@@ -676,6 +676,8 @@ frame if FRAME is nil, and to 1 if AMT is nil."
 
 (use-package evil-multiedit
   :after evil
+  :custom-face
+  (iedit-occurrence ((t (:box (:line-width (-1 . -1)) :inherit nil :style nil))))
   :general
   ('visual "R" 'evil-multiedit-match-all)
   ("M-d" 'evil-multiedit-match-and-next)
