0583d9e6cd326fd57e8c3d011d9065ec8d8c05c3
Nasser Alkmim <nasser.alkmim@gmail.com>
2023-09-18 14:35:40 +0200

    Change cape

--- init.el
+++ init.el
@@ -1648,7 +1648,7 @@ When matching, reference is stored in match group 1."
   :demand
   :after corfu
   :general
-  ('normal "M-/" 'cape-dabbrev)
+  ('insert "M-/" 'cape-dabbrev)
   :config
   ;; Add `completion-at-point-functions', used by `completion-at-point'.
   (add-to-list 'completion-at-point-functions #'cape-file)
