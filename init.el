3973200bb9594d4a5f4f1eef828993b5ea7bdf3d
Nasser Alkmim <nasser.alkmim@gmail.com>
2023-11-13 11:27:35 +0100

    Update elpaca

--- init.el
+++ init.el
@@ -2,7 +2,7 @@
   "Time when Emacs was started")
 
 ;; Bootstrap elpaca
-(defvar elpaca-installer-version 0.5)
+(defvar elpaca-installer-version 0.6)
 (defvar elpaca-directory (expand-file-name "elpaca/" user-emacs-directory))
 (defvar elpaca-builds-directory (expand-file-name "builds/" elpaca-directory))
 (defvar elpaca-repos-directory (expand-file-name "repos/" elpaca-directory))
