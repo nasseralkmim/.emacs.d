db0dc5208bf02425aa79945f465f4a7322a6c609
Nasser Alkmim <nasser.alkmim@gmail.com>
2024-01-26 14:28:09 +0100

    Disable inline flymake

--- init.el
+++ init.el
@@ -790,7 +790,7 @@ frame if FRAME is nil, and to 1 if AMT is nil."
   :config
   ;; delay check, check only on save
   (setq flymake-no-changes-timeout 1                 ;only when saved
-        flymake-show-diagnostics-at-end-of-line 'short ; just use "M-n"
+        flymake-show-diagnostics-at-end-of-line nil ; just use "M-n"
         flymake-mode-line-lighter "Fly")
   ;; avoid warning in the 'flymake' log
   (remove-hook 'flymake-diagnostic-functions 'flymake-proc-legacy-flymake))
