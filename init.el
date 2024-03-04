b1aa8dc25f6d9f74e6b7b5f1191a8a0813eb3f12
Nasser Alkmim <nasser.alkmim@gmail.com>
2024-03-04 13:25:16 +0100

    Change org download

--- init.el
+++ init.el
@@ -1609,6 +1609,7 @@ When matching, reference is stored in match group 1."
   :init
   (setq
    org-download-image-dir "./images"
+   org-download-annotate-function nil
    org-download-image-html-width 350
    org-download-image-latex-width 10)
   ;; can resize screen shot
