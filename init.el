c7e01e92c766748d861a156524567a8eefcc1b5d
Nasser Alkmim <nasser.alkmim@gmail.com>
2024-03-20 08:21:35 +0100

    Update latex

--- init.el
+++ init.el
@@ -1899,8 +1899,6 @@ When matching, reference is stored in match group 1."
         :build (:not elpaca--compile-info) ;; Make will take care of this step
         :files ("*.el" "doc/*.info*" "etc" "images" "latex" "style")
         :version (lambda (_) (require 'tex-site) AUCTeX-version))
-  :after tex-mode
-  :mode ("\\.tex\\'" . LaTeX-mode)
   :commands TeX-command-sentinel
   :custom-face
   (font-latex-sectioning-1-face ((t (:slant oblique :box t :height 1.0))))
