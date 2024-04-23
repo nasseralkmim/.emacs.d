08d219532670173d352bc90fc2e6e8d892df116e
Nasser Alkmim <nasser.alkmim@gmail.com>
2024-04-23 15:49:22 +0200

    Add gnus score for rss

--- init.el
+++ init.el
@@ -3936,7 +3936,8 @@ its results, otherwise display STDERR with
         gnus-home-directory "~/Sync/news/" ; easier to sync different machines with git
         gnus-home-score-file '(("Science" "~/Sync/news/papers.SCORE")
                                ("Wiley" "~/Sync/news/papers.SCORE")
-                               ("arXiv" "~/Sync/news/papers.SCORE"))
+                               ("arXiv" "~/Sync/news/papers.SCORE")
+                               ("rss" "~/Sync/news/rssnews.SCORE"))
         gnus-kill-files-directory "~/Sync/news/" ; to store the score
         ;; Attempts to make it faster
         gnus-fetch-old-headers nil       ; build from already read mail, nil is faster, use '^' to get parent
