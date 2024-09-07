ad3f7422dc72dc5f9093adcde2eb9f70d1075c75
Nasser Alkmim <nasser.alkmim@gmail.com>
2024-09-07 07:36:11 +0200

    nerd-icons: update

--- init.el
+++ init.el
@@ -680,55 +680,8 @@ frame if FRAME is nil, and to 1 if AMT is nil."
   (all-the-icons-scale-factor 1))
 
 (use-package nerd-icons
-  :ensure (nerd-icons :host github :repo "rainstormstudio/nerd-icons.el"
-                      :files (:defaults "data"))
   :defer 1
-  :demand ;require
-  :custom
-  ;; need to install the nerd-font
-  ;; For kitty terminal need to add family to kitty config (C-S-<f2>)
-  (nerd-icons-font-family "Symbols Nerd Font Mono")
-  :config
-  ;; to use with corfu and kind-icon
-  (setq kind-icon-use-icons nil)
-  (setq kind-icon-mapping
-        `(
-          (array ,(nerd-icons-codicon "nf-cod-symbol_array") :face font-lock-type-face)
-          (boolean ,(nerd-icons-codicon "nf-cod-symbol_boolean") :face font-lock-builtin-face)
-          (class ,(nerd-icons-codicon "nf-cod-symbol_class") :face font-lock-type-face)
-          (color ,(nerd-icons-codicon "nf-cod-symbol_color") :face success)
-          (command ,(nerd-icons-codicon "nf-cod-terminal") :face default)
-          (constant ,(nerd-icons-codicon "nf-cod-symbol_constant") :face font-lock-constant-face)
-          (constructor ,(nerd-icons-codicon "nf-cod-triangle_right") :face font-lock-function-name-face)
-          (enummember ,(nerd-icons-codicon "nf-cod-symbol_enum_member") :face font-lock-builtin-face)
-          (enum-member ,(nerd-icons-codicon "nf-cod-symbol_enum_member") :face font-lock-builtin-face)
-          (enum ,(nerd-icons-codicon "nf-cod-symbol_enum") :face font-lock-builtin-face)
-          (event ,(nerd-icons-codicon "nf-cod-symbol_event") :face font-lock-warning-face)
-          (field ,(nerd-icons-codicon "nf-cod-symbol_field") :face font-lock-variable-name-face)
-          (file ,(nerd-icons-codicon "nf-cod-symbol_file") :face font-lock-string-face)
-          (folder ,(nerd-icons-codicon "nf-cod-folder") :face font-lock-doc-face)
-          (interface ,(nerd-icons-codicon "nf-cod-symbol_interface") :face font-lock-type-face)
-          (keyword ,(nerd-icons-codicon "nf-cod-symbol_keyword") :face font-lock-keyword-face)
-          (macro ,(nerd-icons-codicon "nf-cod-symbol_misc") :face font-lock-keyword-face)
-          (magic ,(nerd-icons-codicon "nf-cod-wand") :face font-lock-builtin-face)
-          (method ,(nerd-icons-codicon "nf-cod-symbol_method") :face font-lock-function-name-face)
-          (function ,(nerd-icons-codicon "nf-cod-symbol_method") :face font-lock-function-name-face)
-          (module ,(nerd-icons-codicon "nf-cod-file_submodule") :face font-lock-preprocessor-face)
-          (numeric ,(nerd-icons-codicon "nf-cod-symbol_numeric") :face font-lock-builtin-face)
-          (operator ,(nerd-icons-codicon "nf-cod-symbol_operator") :face font-lock-comment-delimiter-face)
-          (param ,(nerd-icons-codicon "nf-cod-symbol_parameter") :face default)
-          (property ,(nerd-icons-codicon "nf-cod-symbol_property") :face font-lock-variable-name-face)
-          (reference ,(nerd-icons-codicon "nf-cod-references") :face font-lock-variable-name-face)
-          (snippet ,(nerd-icons-codicon "nf-cod-symbol_snippet") :face font-lock-string-face)
-          (string ,(nerd-icons-codicon "nf-cod-symbol_string") :face font-lock-string-face)
-          (struct ,(nerd-icons-codicon "nf-cod-symbol_structure") :face font-lock-variable-name-face)
-          (text ,(nerd-icons-codicon "nf-cod-text_size") :face font-lock-doc-face)
-          (typeparameter ,(nerd-icons-codicon "nf-cod-list_unordered") :face font-lock-type-face)
-          (type-parameter ,(nerd-icons-codicon "nf-cod-list_unordered") :face font-lock-type-face)
-          (unit ,(nerd-icons-codicon "nf-cod-symbol_ruler") :face font-lock-constant-face)
-          (value ,(nerd-icons-codicon "nf-cod-symbol_field") :face font-lock-builtin-face)
-          (variable ,(nerd-icons-codicon "nf-cod-symbol_variable") :face font-lock-variable-name-face)
-          (t ,(nerd-icons-codicon "nf-cod-code") :face font-lock-warning-face))))
+  :demand t)
 
 (use-package nerd-icons-dired
   :ensure (nerd-icons-dired :type git :host github :repo "rainstormstudio/nerd-icons-dired")
@@ -742,6 +695,9 @@ frame if FRAME is nil, and to 1 if AMT is nil."
   :after (nerd-icons marginalia)
   :init
   (nerd-icons-completion-mode))
+
+(use-package nerd-icons-ibuffer
+  :hook (ibuffer-mode . nerd-icons-ibuffer-mode))
 
 ;; automatic insert matching pairs and navigation
 ;; highlight matching parens
@@ -2977,7 +2933,7 @@ opening a file from dired. Otherwise just regular dired."
   :ensure (svg-lib :type git :host github :repo "rougier/svg-lib"))
 
 ;; Icons for completion in region.
-(use-package kind-icon
+(use-package kind-icon :disabled
   :ensure (kind-icon :type git :host github :repo "jdtsmith/kind-icon")
   :after corfu 
   :custom
