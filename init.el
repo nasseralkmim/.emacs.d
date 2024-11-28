4b051445974a7b18b7a240565e2f7301ff217e4d
Nasser Alkmim <nasser.alkmim@gmail.com>
2024-11-28 09:55:56 +0100

    org: use main org

--- init.el
+++ init.el
@@ -1031,7 +1031,7 @@ org-mode"
 (use-package org
   ;; Since elpaca queue fist before loading, we need to wait here.
   ;; So we load the correct version of org instead of built-in when exporting async.
-  :ensure (org :repo "https://code.tecosaur.net/tec/org-mode.git")
+  ;; :ensure (org :repo "https://code.tecosaur.net/tec/org-mode.git")
   :mode (("\\.org$" . org-mode))
   :custom-face
   (org-block ((t (:inherit org-agenda-restriction-lock))))
