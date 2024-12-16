1b878920342f7ce64446584af644dc3e91041518
Nasser Alkmim <nasser.alkmim@gmail.com>
2024-12-16 15:31:13 +0100

    change font

--- early-init.el
+++ early-init.el
@@ -27,7 +27,7 @@
    ;; (right-fringe . 8)                   ;; Thin right fringe
    (tool-bar-lines . 0)                 ;; No tool bar
    (vertical-scroll-bars . nil)
-   (font . "Monaspace Neon 14")))
+   (font . "Monaspace Neon")))
 
 ;; from: https://github.com/SystemCrafters/rational-emacs/blob/master/early-init.el
 ;; native compilation settings
