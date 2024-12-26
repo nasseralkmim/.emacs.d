b51c2870ee54a734d23c802af5ad0946339c3cda
Nasser Alkmim <nasser.alkmim@gmail.com>
2024-12-26 18:12:45 +0100

    aider: update

--- init.el
+++ init.el
@@ -4716,7 +4716,11 @@ RESCHEDULE-FN is the function to reschedule."
   ("C-c C-a" . aider-transient-menu)
   :config
   ;; (setq aider-args '("--model" "ollama_chat/llama3.3"))
-  (setq aider-args '("--model" "perplexity/llama-3.1-sonar-large-128k-online" "--map-tokens" "1024"))
+  (setq aider-args '("--model"
+                     "perplexity/llama-3.1-sonar-large-128k-online"
+                     "--map-tokens" "2048"
+                     "--architect"
+                     "--watch-files"))
   (setenv "OLLAMA_API_BASE" "http://127.0.0.1:11434")
   (setenv "PERPLEXITYAI_API_KEY" (funcall (plist-get (car (auth-source-search :host "api.perplexity.com")) :secret))))
 
