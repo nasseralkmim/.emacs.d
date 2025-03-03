f219f700cbfa8e34da75872fbfadf35b58f2baa8
Nasser Alkmim <nasser.alkmim@gmail.com>
2025-03-03 11:13:16 +0100

    aider: update

--- init.el
+++ init.el
@@ -4836,14 +4836,11 @@ RESCHEDULE-FN is the function to reschedule."
   :bind
   ("C-c C-a" . aidermacs-transient-menu)
   :config
-  ;; (setq aidermacs-default-model "gemini/gemini-2.0-flash-exp")
-  ;; (setq aidermacs-default-model "openrouter/anthropic/claude-3.5-sonnet")
-  (setq aidermacs-default-model "openrouter/deepseek/deepseek-r1")
+  (setq aidermacs-default-model "gemini/gemini-2.0-flash-exp")
   
   (setenv "OLLAMA_API_BASE" "http://127.0.0.1:11434")
   (setenv "PERPLEXITYAI_API_KEY" (funcall (plist-get (car (auth-source-search :host "api.perplexity.com")) :secret)))
   (setenv "ANTHROPIC_API_KEY" (funcall (plist-get (car (auth-source-search :host "api.anthropic.com")) :secret)))
-  (setenv "OPENROUTER_API_KEY" (funcall (plist-get (car (auth-source-search :host "api.openrouter.com")) :secret)))
   (setenv "OPENAI_API_KEY" (funcall (plist-get (car (auth-source-search :host "api.github.com" :user "nasseralkmim^gptel")) :secret)))
   (setenv "OPENAI_API_BASE" "https://models.inference.ai.azure.com")
   (setenv "GEMINI_API_KEY" (funcall (plist-get (car (auth-source-search :host "api.gemini.com")) :secret))))
