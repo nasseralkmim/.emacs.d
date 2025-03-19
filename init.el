f0123f88f2d425bfb7bf52bc55c164348c4023b2
Nasser Alkmim <nasser.alkmim@gmail.com>
2025-03-19 12:58:08 +0100

    preview: feat: Adjust preview scale

--- init.el
+++ init.el
@@ -1937,7 +1937,7 @@ When matching, reference is stored in match group 1."
   ;; It is ok if we can fold the table or algorithm.
   (setq preview-default-option-list '("displaymath" "showlabels" "textmath")
         preview-auto-cache-preamble t
-        preview-scale-function 1.3
+        preview-scale-function 1.01659593
         ;; preview-LaTeX-command-replacements '(preview-LaTeX-disable-pdfoutput)
         )
   (add-to-list 'preview-auto-reveal-commands 'meow-left)
