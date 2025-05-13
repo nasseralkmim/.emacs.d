fbf3a2729a70f60e6f106986fab6ce5fbc9f1f1f
Nasser Alkmim <nasser.alkmim@gmail.com>
2025-05-13 07:54:34 +0200

    Add shell session buffer opening after execute

--- init.el
+++ init.el
@@ -1351,7 +1351,7 @@ graphics."
           (:eval . "never-export") ; don't eval blocks when exporting, except when `:eval yes`
           (:exports . "results")))) ; export only plots by default
 
-(use-package ob-python-open-session-buffer-after-execute
+(use-package ob-python-open-session-buffer-after-execute-hack
   :ensure nil
   :after org
   :init
@@ -1364,6 +1364,20 @@ graphics."
           (display-buffer buffer nil 'visible)))))
 
   (add-hook 'org-babel-after-execute-hook 'org-babel-python-open-session-buffer-after-execute))
+
+(use-package ob-shell-open-session-buffer-after-execute-hack
+  :ensure nil
+  :after org
+  :init
+  (defun org-babel-shell-open-session-buffer-after-execute ()
+    "Open the shell session buffer after executing the code block."
+    (let ((session-buffer (format "*%s*" (cdr (assoc :session (nth 2 (org-babel-get-src-block-info)))))))
+      (dolist (buffer (buffer-list))
+        (when (string= (buffer-name buffer) session-buffer)
+          ;; open session buffer but don't move cursor to it
+          (display-buffer buffer nil 'visible)))))
+
+  (add-hook 'org-babel-after-execute-hook 'org-babel-shell-open-session-buffer-after-execute))
 
 (use-package ob-core
   :ensure nil
