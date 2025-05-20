6bae1af85ad1c3fc9fe4465b9623afe524fe0f7e
Nasser Alkmim <nasser.alkmim@gmail.com>
2025-05-20 23:35:23 +0200

    chore: Configure automagic-dark-mode settings

--- init.el
+++ init.el
@@ -5072,6 +5072,9 @@ If called with a prefix argument, prompt for a context string."
 
 (use-package automagic-dark-mode
   :ensure (automagic-dark-mode :url  "https://github.com/sstraust/automagic-dark-mode")
-  :bind ("<f5>" . automagic-dark-mode))
+  :bind ("<f5>" . automagic-dark-mode)
+  :config
+  (setq automagic-dark-wcag-ratio 1.
+        automagic-dark-luminance-inversion-exp 0.3))
 
 (message "Start up time %.2fs" (float-time (time-subtract (current-time) my-start-time)))
