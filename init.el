1a8cac8ffc4bb32bc0ba2f8c31ca3ee65ce55203
Nasser Alkmim <nasser.alkmim@gmail.com>
2025-05-22 11:10:05 +0200

    chore: Adjust automagic-dark luminance inversion exponent

--- init.el
+++ init.el
@@ -5089,6 +5089,6 @@ If called with a prefix argument, prompt for a context string."
   :bind ("<f5>" . automagic-dark-mode)
   :config
   (setq automagic-dark-wcag-ratio 1
-        automagic-dark-luminance-inversion-exp 0.5))
+        automagic-dark-luminance-inversion-exp 0.8))
 
 (message "Start up time %.2fs" (float-time (time-subtract (current-time) my-start-time)))
