f5564f17f3742c4eb100de4dbede4ddb3479b27c
Nasser Alkmim <nasser.alkmim@gmail.com>
2025-06-25 11:15:13 +0200

    refactor(elpaca): Remove unnecessary version lambda for AUCTeX package

--- init.el
+++ init.el
@@ -1902,8 +1902,7 @@ When matching, reference is stored in match group 1."
                      "--with-packagedatadir=./")
                     ("make"))
         :build (:not elpaca--compile-info) ;; Make will take care of this step
-        :files ("*.el" "doc/*.info*" "etc" "images" "latex" "style")
-        :version (lambda (_) (require 'tex-site) AUCTeX-version))
+        :files ("*.el" "doc/*.info*" "etc" "images" "latex" "style"))
   :init
   ;; This commit add a remap from LaTeX-mode (which Auctex) uses to latex-mode
   ;; [[orgit-rev:~/.local/src/emacs/::1ea3b369021c90701c634c512426f75ce1291d77][~/.local/src/emacs/ (magit-rev 1ea3b369021)]]
