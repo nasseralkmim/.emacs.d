97b6a4372202513b0aab6d5e2a1e44524b9f93dc
Nasser Alkmim <nasser.alkmim@gmail.com>
2025-09-13 07:34:49 +0200

    chore(init.el): Add openai/gpt-5-nano to models list

--- init.el
+++ init.el
@@ -4345,7 +4345,7 @@ If an edraw editor is active for this link, preview is skipped."
   :endpoint "/api/v1/chat/completions"
   :stream t
   :key (funcall (plist-get (car (auth-source-search :host "api.openrouter.com")) :secret))
-  :models '(openai/gpt-5 openai/gpt-5-mini openai/gpt-4.1-nano))
+  :models '(openai/gpt-5 openai/gpt-5-mini openai/gpt-5-nano openai/gpt-4.1-nano))
   ;; set the default
   (setq gptel-backend (gptel-make-openai "OpenRouter"               ;Any name you want
                         :host "openrouter.ai"
