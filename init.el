06086d4945df6f12a1224151c1894d096576df2c
Nasser Alkmim <nasser.alkmim@gmail.com>
2025-09-16 11:19:10 +0200

    fix(main): Remove unused dired-mode hook in diff-hl configuration

--- init.el
+++ init.el
@@ -2795,10 +2795,7 @@ Only if there is more than one window opened."
 (use-package diff-hl
   :hook
   (prog-mode . diff-hl-mode)
-  (magit-post-refresh . diff-hl-magit-post-refresh)
-  ;; (dired-mode . diff-hl-dired-mode)
-  :demand
-  :after magit)
+  (magit-post-refresh . diff-hl-magit-post-refresh))
 
 ;; built in substitute for `list-buffer`
 (use-package ibuffer
