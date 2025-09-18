c3da82f7c15b988144eccbb07ba16c70f21658f7
Nasser Alkmim <nasser.alkmim@gmail.com>
2025-09-18 07:23:47 +0200

    style(change hl-line background to unspecified in init.el)

--- init.el
+++ init.el
@@ -2261,7 +2261,7 @@ Otherwise, toggle only the directory at point."
       (with-eval-after-load 'breadcrumb
         (set-face-attribute 'breadcrumb-project-leaf-face nil :inherit 'default))
       (with-eval-after-load 'hl-line
-        (set-face-attribute 'hl-line nil :background (modus-themes-get-color-value 'bg-hl-line) :box '(:color "grey40" :line-width (-1 . -1)) :extend t))
+        (set-face-attribute 'hl-line nil :background 'unspecified :box '(:color "grey40" :line-width (-1 . -1)) :extend t))
       (modus-themes-with-colors
         ;; The `org-src-block-faces' does not get re-applied in existing
         ;; Org buffers.  Do M-x org-mode-restart for changes to take
