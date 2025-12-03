686f00a56ff84ba9f07922fe842d520cc02bc369
Nasser Alkmim <nasser.alkmim@gmail.com>
2025-12-03 15:00:49 +0100

    refactor(init): use :init instead of :config for shr setup
    - Move shr configuration from :config to :init to run earlier during
    - init Maintain behavior of disabling colors for unreadable gnus in
    - dark mode

--- init.el
+++ init.el
@@ -3972,7 +3972,7 @@ its results, otherwise display STDERR with
 
 (use-package shr
   :ensure nil
-  :config
+  :init
   ;; avoid unreadable colors in gnus when in dark mode
   (setq shr-use-colors nil))
 
