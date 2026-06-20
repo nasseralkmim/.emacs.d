42072972cac379ff4250dc9af0d1d951ec1514da
Nasser Alkmim <nasser.alkmim@gmail.com>
2026-06-20 07:20:02 +0200

    feat(latex): enable preview-auto in terminal environments
    Remove the graphic display constraint to allow LaTeX math previews
    in terminal-based Emacs sessions.

--- init.el
+++ init.el
@@ -1761,7 +1761,6 @@ When matching, reference is stored in match group 1."
 
 ;; Better math preview in latex
 (use-package preview-auto
-  :if (display-graphic-p)
   :vc (:url "https://github.com/ultronozm/preview-auto.el")
   :hook (LaTeX-mode . preview-auto-mode)
   :config
